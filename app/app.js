85ab620fa0e7f9337c83ac9af26c59912051e4ea
Katarina Jovanovic <kacaj91@gmail.com>
2015-11-21 22:39:23 +0100

    Routing

--- app/app.js
+++ app/app.js
@@ -3,7 +3,30 @@
 	"use strict";
 	var app = angular.module('productManagement', 
 							 ['common.services',
+							  'ui.router',
 							  'productResourceMock']);
+							  
+	
+	app.config(["$stateProvider", "$urlRouterProvider",
+				function($stateProvider, $urlRouterProvider){
+						$urlRouterProvider.otherwise("/");
+						$stateProvider
+								.state("home", {
+									url: "/",
+									templateUrl: "app/welcomeView.html"
+								})
+								.state("productList", {
+									url: "/products",
+									templateUrl: "app/products/productListView.html",
+									controller: "ProductListCtrl as vm"
+								})
+								.state("productEdit", {
+									url: "/products/edit/:productId",
+									templateUrl: "app/products/productListView.html",
+									controller: "ProductEditCtrl as vm"
+								})
+					
+				}]);
 	
 }());
 

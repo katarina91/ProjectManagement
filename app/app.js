898299d01b5d07c890ca26249db9e57297b5cbf3
Katarina Jovanovic <kacaj91@gmail.com>
2015-11-21 16:38:12 +0100

    Accessing Data

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 
 (function(){
 	"use strict";
-	var app = angular.module('productManagement', []);
+	var app = angular.module('productManagement', 
+							 ['common.services',
+							  'productResourceMock']);
 	
 }());
 
---
+++ common/services/productResource.js
@@ -0,0 +1,17 @@
+
+(function (){
+	"use strict";
+	
+	angular
+		.module("common.services")
+		.factory("productResource",
+				 ["$resource",
+				 productResource]);
+				
+				
+	function productResource ($resource)
+	{
+		return $resource("/app/products/:productId");
+	}
+	
+}());
